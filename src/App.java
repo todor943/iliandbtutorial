6a350c5bb799badd8af886498f015e6f0bd47ec1
Todor Markov <todorator@gmail.com>
2021-04-04 23:07:56 +0300

    password and port

--- src/App.java
+++ src/App.java
@@ -7,8 +7,8 @@ public class App {
       try {
          Class.forName("org.postgresql.Driver");
          c = DriverManager
-            .getConnection("jdbc:postgresql://localhost:5432/testdb",
-            "postgres", "123");
+            .getConnection("jdbc:postgresql://localhost:5432/postgres",
+            "postgres", "master");
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName()+": "+e.getMessage());
