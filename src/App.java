460ed13ad5c35ed4f567e3a4e924ecc6fdda20a6
Todor Markov <todorator@gmail.com>
2021-04-04 23:44:01 +0300

    latest

--- src/App.java
+++ src/App.java
@@ -1,19 +1,38 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class App {
    public static void main(String args[]) {
       Connection c = null;
+      Statement stmt = null;
       try {
          Class.forName("org.postgresql.Driver");
          c = DriverManager
             .getConnection("jdbc:postgresql://localhost:5432/postgres",
             "postgres", "master");
+
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+         stmt = c.createStatement();
+         ResultSet rs = stmt.executeQuery( "SELECT * FROM mytable;" );
+         while ( rs.next() ) {
+            String bla = rs.getString("bla");
+            System.out.println(bla);
+         }
+         rs.close();
+         stmt.close();
+         c.close();
+
+         System.out.println("Closed database successfully");
+
       } catch (Exception e) {
          e.printStackTrace();
          System.err.println(e.getClass().getName()+": "+e.getMessage());
          System.exit(0);
       }
-      System.out.println("Opened database successfully");
+      System.out.println("Finished successfully");
    }
 }
